8e9ba2ea20b7cd37549e2defc80ecc513a333204
Ankit Sardesai <amsardesai@gmail.com>
2014-01-19 15:14:28 -0500

    Created customized map and header bar

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,12 +1,113 @@
 
+
 var initialize = function() {
 
+	var styleArray = [
+		{
+			"featureType": "road",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "transit",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "poi",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "administrative",
+			"elementType": "geometry",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "administrative.country",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#AAAAAA" },
+				{ "weight": 1 }
+			]
+		},{
+			"featureType": "administrative.province",
+			"elementType": "geometry.stroke",
+			"stylers": [
+				{ "visibility": "on" },
+				{ "color": "#CCCCCC" },
+				{ "weight": 0.7 }
+			]
+		},{
+			"featureType": "administrative.locality",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "administrative.neighborhood",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "administrative.land_parcel",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		},{
+			"featureType": "landscape",
+			"stylers": [
+				{ "color": "#FFFFFF" }
+			]
+		},{
+			"featureType": "water",
+			"stylers": [
+				{ "color": "#75B4FF" }
+			]
+		},{
+			"elementType": "labels",
+			"stylers": [
+				{ "visibility": "off" }
+			]
+		}
+	];
+
+
 	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
-		zoom: 8
+		center: new google.maps.LatLng(40, -97),
+		zoom: 4,
+		panControl: true,
+		panControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_TOP
+		},
+		zoomControl: true,
+		zoomControlOptions: {
+			style: google.maps.ZoomControlStyle.SMALL,
+			position: google.maps.ControlPosition.RIGHT_TOP
+		},
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		rotateControl: false,
+		overviewMapControl: false
 	};
 
 	var map = new google.maps.Map(document.getElementById("mapcanvas"), mapOptions);
+	map.setOptions({ styles: styleArray });
+
+	var emptyDiv = document.createElement('div');
+	emptyDiv.className = 'controlOffset';
+	emptyDiv.index = 0;
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(emptyDiv);
+
+	// Limit the zoom level
+	var minZoomLevel = 3;
+	var maxZoomLevel = 7;
+	google.maps.event.addListener(map, 'zoom_changed', function() {
+		if (map.getZoom() < minZoomLevel) map.setZoom(minZoomLevel);
+		if (map.getZoom() > maxZoomLevel) map.setZoom(maxZoomLevel);
+	});
 
 };
 
